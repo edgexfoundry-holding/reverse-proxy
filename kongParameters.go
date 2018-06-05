1793f3535ebd0be4ee1bd315268a6e4f1186f447
tingyu.zeng@rsa.com <tingyu.zeng@rsa.com>
2018-06-05 11:24:24 -0400

    enabling JWT and adding JWT creation for admin services. adding cert retrieving and pushing to kong.
    Signed-off-by: tingyu.zeng@rsa.com <tingyu.zeng@rsa.com>

--- kongParameters.go
+++ kongParameters.go
@@ -1,9 +1,27 @@
+/*******************************************************************************
+ * Copyright 2018 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author: Tingyu Zeng, Dell
+ * @version: 0.1.0
+ *******************************************************************************/
 package main
 
+import jwt "github.com/dgrijalva/jwt-go"
+
 type KongService struct {
-	Name string `url:"name,omitempty"`
-	Host string `url:"host,omitempty"`
-	Port string	`url:"port,omitempty"`
+	Name     string `url:"name,omitempty"`
+	Host     string `url:"host,omitempty"`
+	Port     string `url:"port,omitempty"`
 	Protocol string `url:"protocol,omitempty"`
 }
 
@@ -15,8 +33,8 @@ type KongPlugin struct {
 	Name string `url:"name,omitempty"`
 }
 
-type KongBasicAuthPlugin struct{
-	Name string `url:"name,omitempty"`
+type KongBasicAuthPlugin struct {
+	Name            string `url:"name,omitempty"`
 	HideCredentials string `url:"config.hide_credentials,omitempty"`
 }
 
@@ -25,8 +43,31 @@ type KongUser struct {
 	Password string `url:"password,omitempty"`
 }
 
-type KongCert struct {
-	Cert string `url:cerpath,omitempty`
-	Key string  `url:keypath, omitempty`
-	Snis string `url:snis,  omitempty`
+type Data struct {
+	Cert string
+	Key  string
+}
+
+type Cert struct {
+	Data Data
+}
+
+type CertInfo struct {
+	Cert string   `json:"cert,omitempty"`
+	Key  string   `json:"key,omitempty"`
+	Snis []string `json:"snis,omitempty"`
+}
+
+type JWTCred struct {
+	ConsumerID string `json:"consumer_id, omitempty"`
+	CreatedAt  int    `json:"created_at, omitempty"`
+	ID         string `json:"id, omitempty"`
+	Key        string `json:"key, omitempty"`
+	Secret     string `json:"secret, omitempty"`
+}
+
+type KongJWTClaims struct {
+	ISS  string `json:"iss"`
+	Acct string `json:"account"`
+	jwt.StandardClaims
 }
--- main.go
+++ main.go
@@ -1,18 +1,39 @@
+/*******************************************************************************
+ * Copyright 2018 Dell Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * @author: Tingyu Zeng, Dell
+ * @version: 0.1.0
+ *******************************************************************************/
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/dghubble/sling"
+	"github.com/dgrijalva/jwt-go"
 )
 
 const (
 	servicesPath     = "services/"
 	consumersPath    = "consumers/"
 	certificatesPath = "certificates/"
+	vaultToken       = "X-Vault-Token"
 )
 
 func main() {
@@ -20,11 +41,12 @@ func main() {
 	//TODO: get data from consul to overwrite the local default config
 	//TODO: change the log format from console output to log service of edgex
 	config := LoadTomlConfig("res/configuration.toml")
+	proxyBaseURL := fmt.Sprintf("http://%s:%s/", config.KongURL.Server, config.KongURL.AdminPort)
+	secretServiceBaseURL := fmt.Sprintf("http://%s:%s/", config.SecretService.Server, config.SecretService.Port)
+	client := &http.Client{Timeout: 10 * time.Second}
 
-	baseURL := fmt.Sprintf("http://%s:%s/", config.KongUrl.Server, config.KongUrl.AdminPort)
-	client := &http.Client{}
-
-	checkKongStatus(baseURL, client)
+	checkProxyStatus(proxyBaseURL, client)
+	checkSecretServiceStatus(secretServiceBaseURL+config.SecretService.HealthcheckPath, client)
 
 	for _, service := range config.EdgexServices {
 		serviceParams := &KongService{
@@ -34,10 +56,9 @@ func main() {
 			Protocol: service.Protocol,
 		}
 
-		initKongServices(baseURL, client, serviceParams)
-
+		initKongService(proxyBaseURL, client, serviceParams)
 		jwtServicePath := fmt.Sprintf("services/%s/plugins", service.Name)
-		initKongJWT(baseURL, client, jwtServicePath, service.Name)
+		initJWTAuthForService(proxyBaseURL, client, jwtServicePath, service.Name)
 	}
 
 	for _, service := range config.EdgexServices {
@@ -45,47 +66,48 @@ func main() {
 			Paths: []string{"/" + service.Name},
 		}
 		routePath := fmt.Sprintf("services/%s/routes", service.Name)
-		initKongRoutes(baseURL, client, routeParams, routePath, service.Name)
+		initKongRoutes(proxyBaseURL, client, routeParams, routePath, service.Name)
 	}
 
-	initKongAdminInterface(config, baseURL, client)
-	loadKongCerts(baseURL, client)
-	os.Exit(0)
+	initKongAdminInterface(config, proxyBaseURL, client)
+	//loadKongCerts(config, proxyBaseURL, client)
+
 }
 
-func checkKongStatus(url string, c *http.Client) {
+func checkProxyStatus(url string, c *http.Client) {
 	req, err := sling.New().Get(url).Request()
 	resp, err := c.Do(req)
 	if err != nil {
-		fmt.Println("KONG is failed to start up, please verify...")
+		fmt.Println("The status of reverse proxy is unknown, the initialization is terminated.")
 		os.Exit(0)
 	} else {
 		fmt.Println(resp.StatusCode)
 		if resp.StatusCode == 200 {
-			fmt.Println("Ping successful with KONG service.")
+			fmt.Println("Reverse proxy is up successfully.")
 		} else {
-			fmt.Println("Failed to ping KONG service. Please check KONG service status.")
+			fmt.Println("The status of reverse proxy is unknown, the initialization is terminated.")
+			os.Exit(0)
 		}
 	}
 }
 
-func initKongServices(url string, c *http.Client, service *KongService) {
+func initKongService(url string, c *http.Client, service *KongService) {
 	req, err := sling.New().Base(url).Post(servicesPath).BodyForm(service).Request()
 	resp, err := c.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		fmt.Println("Failed to set up service for " + service.Name)
+		fmt.Println(fmt.Sprintf("Failed to set up proxy service for %s.", service.Name))
 	} else {
 		fmt.Println(resp.StatusCode)
 		if resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println("Successful to set up service for " + service.Name)
+			fmt.Println(fmt.Sprintf("Successful to set up proxy service for %s.", service.Name))
 		} else {
-			fmt.Println("Failed to set up service for " + service.Name)
+			fmt.Println(fmt.Sprintf("Failed to set up proxy service for %s.", service.Name))
 		}
 	}
 }
 
-func initKongJWT(url string, c *http.Client, path string, name string) {
+func initJWTAuthForService(url string, c *http.Client, path string, name string) {
 	jwtParams := &KongPlugin{
 		Name: "jwt",
 	}
@@ -94,13 +116,13 @@ func initKongJWT(url string, c *http.Client, path string, name string) {
 	resp, err := c.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		fmt.Println("Failed to set up jwt authentication for service " + name)
+		fmt.Println(fmt.Sprintf("Failed to set up jwt authentication for service %s.", name))
 	} else {
 		fmt.Println(resp.StatusCode)
 		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println("Successful to set up jwt authentication for service " + name)
+			fmt.Println(fmt.Sprintf("Successful to set up jwt authentication for service %s.", name))
 		} else {
-			fmt.Println("Failed to set up jwt authentication for service " + name)
+			fmt.Println(fmt.Sprintf("Failed to set up jwt authentication for service %s.", name))
 		}
 	}
 }
@@ -113,20 +135,19 @@ func initKongRoutes(url string, c *http.Client, r *KongRoute, path string, name 
 	} else {
 		fmt.Println(resp.StatusCode)
 		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println("Successful to set up route for " + name)
+			fmt.Println(fmt.Sprintf("Successful to set up route for %s.", name))
 		} else {
-			fmt.Println("Failed to set up route for " + name)
+			fmt.Println(fmt.Sprintf("Failed to set up route for %s.", name))
 		}
 	}
-
 }
 
+//redirect request for 8001 to an admin service of 8000, and add authentication
 func initKongAdminInterface(config *tomlConfig, url string, c *http.Client) {
-	//redirect request for 8001 to an admin service of 8000, and add authentication
 	adminServiceParams := &KongService{
 		Name:     "admin",
-		Host:     config.KongUrl.Server,
-		Port:     config.KongUrl.AdminPort,
+		Host:     config.KongURL.Server,
+		Port:     config.KongURL.AdminPort,
 		Protocol: "http",
 	}
 	req, err := sling.New().Base(url).Post(servicesPath).BodyForm(adminServiceParams).Request()
@@ -159,85 +180,131 @@ func initKongAdminInterface(config *tomlConfig, url string, c *http.Client) {
 		}
 	}
 
-	//enable basic-auth for admin service
-	//url -X POST http://kong:8001/services/{service}/plugins \
-	//--data "name=basic-auth"  \
-	//--data "config.hide_credentials=true"
-	basicAuthParams := &KongBasicAuthPlugin{
-		Name:            "basic-auth",
-		HideCredentials: "true",
-	}
-	req, err = sling.New().Base(url).Post("services/admin/plugins/").BodyForm(basicAuthParams).Request()
-	resp, err = c.Do(req)
+	jwtAdminServicePath := "services/admin/plugins"
+	initJWTAuthForService(url, c, jwtAdminServicePath, "admin")
+
+	createConsumer(config.KongAdmin.UserName, url, c, "admin")
+
+	t, err := createJWTForConsumer(config.KongAdmin.UserName, url, c, "admin")
 	if err != nil {
 		fmt.Println(err.Error())
-		fmt.Println("Failed to enable basic-auth for admin service.")
+		fmt.Println("Failed to create jwt token for admin service.")
 	} else {
-		fmt.Println(resp.StatusCode)
-		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println("Successful to enable basic-auth for admin service.")
-		} else {
-			fmt.Println("Failed to enable basic-auth for admin service.")
-		}
+		fmt.Println(fmt.Sprintf("The JWT for consumer %s is: %s. Please keep the jwt for future use.", config.KongAdmin.UserName, t))
 	}
-
-	//create consumer administrator so that it can be used to consume admin service
-	userNameParams := &KongUser{UserName: config.KongAdmin.UserName}
-	req, err = sling.New().Base(url).Post(consumersPath).BodyForm(userNameParams).Request()
-	resp, err = c.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-		fmt.Println("Failed to create consumer for admin service.")
-	} else {
-		fmt.Println(resp.StatusCode)
-		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println("Successful to create consumer for admin service.")
-		} else {
-			fmt.Println("Failed to create consumer for admin service.")
-		}
-	}
-
-	//curl -X POST http://kong:8001/consumers/{consumer}/basic-auth \
-	//--data "username=administrator" \
-	//--data "password=changeme"
-	adminCredential := &KongUser{
-		UserName: config.KongAdmin.UserName,
-		Password: config.KongAdmin.Password,
-	}
-	adminAuthPath := fmt.Sprintf("consumers/%s/basic-auth", config.KongAdmin.UserName)
-	req, err = sling.New().Base(url).Post(adminAuthPath).BodyForm(adminCredential).Request()
-	resp, err = c.Do(req)
-	if err != nil {
-		fmt.Println(err.Error())
-		fmt.Println(fmt.Sprintf("Failed to add credential for consumer %s.", config.KongAdmin.UserName))
-	} else {
-		fmt.Println(resp.StatusCode)
-		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println(fmt.Sprintf("Successful to add credential for consumer %s.", config.KongAdmin.UserName))
-		} else {
-			fmt.Println(fmt.Sprintf("Failed to add credential for consumer %s.", config.KongAdmin.UserName))
-		}
-	}
-
 }
 
-func loadKongCerts(url string, c *http.Client) {
-	certInfo := &KongCert{
-		Cert: "pemcert",
-		Key:  "key",
-		Snis: "",
-	}
-	req, err := sling.New().Base(url).Post(certificatesPath).BodyForm(certInfo).Request()
+func createConsumer(user string, url string, c *http.Client, name string) {
+	userNameParams := &KongUser{UserName: user}
+	req, err := sling.New().Base(url).Post(consumersPath).BodyForm(userNameParams).Request()
 	resp, err := c.Do(req)
 	if err != nil {
 		fmt.Println(err.Error())
-		fmt.Println(fmt.Sprintf("Failed to add certificate with cert path as %s.", "pem-path"))
+		fmt.Println(fmt.Sprintf("Failed to create consumer for %s service.", name))
+		os.Exit(0)
 	} else {
 		fmt.Println(resp.StatusCode)
 		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
-			fmt.Println(fmt.Sprintf("Successful to add cert."))
+			fmt.Println(fmt.Sprintf("Successful to create consumer for %s service.", name))
 		} else {
-			fmt.Println(fmt.Sprintf("Failed to add cert."))
+			fmt.Println(fmt.Sprintf("Failed to create consumer for %s service.", name))
 		}
 	}
+}
+
+func createJWTForConsumer(user string, url string, c *http.Client, name string) (string, error) {
+	jwtCred := JWTCred{}
+	s := sling.New().Set("Content-Type", "application/x-www-form-urlencoded")
+	req, err := s.New().Get(url).Post(fmt.Sprintf("consumers/%s/jwt", user)).Request()
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println(fmt.Sprintf("Failed to create jwt token for consumer %s.", user))
+		return "", errors.New("Error: unable to create JWT")
+	}
+	fmt.Println(resp.StatusCode)
+	if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
+		fmt.Println(fmt.Sprintf("Successful to create JWT for consumer %s.", user))
+		defer resp.Body.Close()
+		json.NewDecoder(resp.Body).Decode(&jwtCred)
+		fmt.Println(fmt.Sprintf("successful on retrieving JWT credential for consumer %s.", user))
+
+		// Create the Claims
+		claims := KongJWTClaims{
+			jwtCred.Key,
+			user,
+			jwt.StandardClaims{
+				Issuer: "edgex",
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		return token.SignedString([]byte(jwtCred.Secret))
+	}
+	fmt.Println(fmt.Sprintf("Failed to create JWT for consumer %s.", user))
+	return "", errors.New("Error: unable to create JWT")
+}
+
+func checkSecretServiceStatus(url string, c *http.Client) {
+	req, err := sling.New().Get(url).Request()
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println("The status of secret service is unknown, the initialization is terminated.")
+		os.Exit(0)
+	} else {
+		fmt.Println(resp.StatusCode)
+		if resp.StatusCode == 200 {
+			fmt.Println("Secret management service is up successfully.")
+		} else {
+			fmt.Println(fmt.Sprintf("Secret management service is down. Please check the status of secret service with endpoint %s.", url))
+			os.Exit(0)
+		}
+	}
+}
+
+func loadKongCerts(config *tomlConfig, url string, c *http.Client) {
+	cert, key, err := getCertKeyPair(config, c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(0)
+	}
+
+	body := &CertInfo{
+		Cert: cert,
+		Key:  key,
+		Snis: []string{config.SecretService.SNIS},
+	}
+	req, err := sling.New().Base(url).Post(certificatesPath).BodyJSON(body).Request()
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println(fmt.Sprintf("Failed to add certificate with cert path as %s.", config.SecretService.CertPath))
+	} else {
+		fmt.Println(resp.StatusCode)
+		if resp.StatusCode == 200 || resp.StatusCode == 201 || resp.StatusCode == 409 {
+			fmt.Println(fmt.Sprintf("Successful to add certificate to the reverse proxy."))
+		} else {
+			fmt.Println(fmt.Sprintf("Failed to add certificate to the reverse proxy."))
+		}
+	}
+}
+
+func getCertKeyPair(config *tomlConfig, c *http.Client) (string, string, error) {
+	certs := Cert{}
+	s := sling.New().Set(vaultToken, config.SecretService.Token)
+	req, err := s.New().Get(config.SecretService.CertPath).Request()
+	resp, err := c.Do(req)
+	if err != nil {
+		fmt.Println(err.Error())
+		fmt.Println(fmt.Sprintf("Failed to retrieve certificate with path as %s.", config.SecretService.CertPath))
+	} else {
+		defer resp.Body.Close()
+		json.NewDecoder(resp.Body).Decode(&certs)
+		fmt.Println(fmt.Sprintf("successful on retrieving certificate from %s.", config.SecretService.CertPath))
+		return certs.Data.Cert, certs.Data.Key, nil
+	}
+	errInfo := fmt.Sprintf("Failed to retrieve certificate with path as %s.", config.SecretService.CertPath)
+	fmt.Println(errInfo)
+	return "", "", errors.New(errInfo)
 }
